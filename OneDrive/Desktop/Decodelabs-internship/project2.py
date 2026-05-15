8a10f164ff4929612a706370ddf5eb8abef86f95
Haroon Ashar <haroonashar12345@gmail.com>
2026-05-15 16:20:11 +0500

    project2

---
+++ OneDrive/Desktop/Decodelabs-internship/project2.py
@@ -0,0 +1,18 @@
+
+total=0
+while True:  
+    a = input("Enter the amount or type 'exit' to finish: ")
+    if a=="exit":
+        print("The total is: ", total)
+        print("goodbye")
+
+        break
+    
+    
+    a=int(a)
+    total=total + a
+    print("The total is: ", total)
+
+
+
+
